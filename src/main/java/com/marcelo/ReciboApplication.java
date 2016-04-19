810781a0daad6d30042ba905d6cc1966bcb0d73d
marcelosfreitas <doriamsfreitas@globomail.com>
2016-04-18 21:51:40 -0300

    Trabalho Recibo

---
+++ src/main/java/com/marcelo/ReciboApplication.java
@@ -0,0 +1,12 @@
+package com.marcelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciboApplication.class, args);
+	}
+}
